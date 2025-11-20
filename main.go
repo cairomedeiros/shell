19e8de50817819d48e5915e8aec12414687fcd85
cairomedeiros <cairomedeiros13@gmail.com>
2025-11-20 13:43:27 -0300

    add direct execution of external commands

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -15,19 +16,22 @@ type CommandStruct struct {
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	text, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Error")
-	}
+	text, _ := reader.ReadString('\n')
 
-	text_tokens := strings.Split(text, " ")
+	text = strings.TrimSpace(text)
+	text_tokens := strings.Fields(text)
 
 	result := CommandStruct{command: text_tokens[0], args: text_tokens[1:]}
-
-	fmt.Println(result.command)
 
 	for index := range result.args {
 		fmt.Printf("%s \n", result.args[index])
 	}
+
+	cmd := exec.Command(result.command, result.args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	cmd.Run()
 
 }
