5b17f6b373b558a13f97a29f2f1e0873004b80c2
cairomedeiros <cairomedeiros13@gmail.com>
2025-11-23 22:13:42 -0300

    replace chained ifs with command map dispatch

--- main.go
+++ main.go
@@ -13,8 +13,25 @@ type CommandStruct struct {
 	args    []string
 }
 
+type cmdFunc map[string]func(args []string)
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+	commands := cmdFunc{
+		"cd": func(args []string) {
+			err := os.Chdir(strings.Join(args, " "))
+			if err != nil {
+				fmt.Println(err)
+			}
+		},
+		"pwd": func(args []string) {
+			cwd, _ := os.Getwd()
+			fmt.Println("cwd:", cwd)
+		},
+		"exit": func(args []string) {
+			os.Exit(1)
+		},
+	}
 
 	for {
 		text, _ := reader.ReadString('\n')
@@ -26,25 +43,15 @@ func main() {
 			continue
 		}
 
-		if text_tokens[0] == "exit" {
-			os.Exit(1)
-		}
-
 		result := CommandStruct{command: text_tokens[0], args: text_tokens[1:]}
 
 		for index := range result.args {
 			fmt.Printf("%s \n", result.args[index])
 		}
 
-		if result.command == "cd" {
-			err := os.Chdir(strings.Join(result.args, " "))
-			if err != nil {
-				fmt.Println(err)
-			}
-		}
-		if result.command == "pwd" {
-			cwd, _ := os.Getwd()
-			fmt.Println("cwd:", cwd)
+		if fn, ok := commands[result.command]; ok {
+			fn(result.args)
+			continue
 		}
 
 		if result.command != "cd" && result.command != "pwd" && result.command != "exit" {
